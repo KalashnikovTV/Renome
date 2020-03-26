58c0131c97608e90604a55292a72d5bb5f7857f5
KalashnikovTV <max.malaschkov@mail.ru>
2020-03-26 13:32:11 +0300

    start discoverer.html

--- app/js/main.js
+++ app/js/main.js
@@ -23,6 +23,14 @@ $(function(){
     $('select').styler();
 
     
+    $('.discoverer__wrapper .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.discoverer__wrapper').find('.tab-item').removeClass('active-tab').hide();
+            $('.discoverer__wrapper .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 });
 
 
